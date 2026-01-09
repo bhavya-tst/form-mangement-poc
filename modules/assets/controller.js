3a09117a90fb368473d69bb90153f0da52f0d8be
bhavya-tst <bhavya.patel@tsttechnology.io>
2026-01-09 14:38:43 +0530

    added origin headers

--- modules/assets/controller.js
+++ modules/assets/controller.js
@@ -1,84 +1,85 @@
-import { getAssetContent } from './service.js';
+// import { getAssetContent } from './service.js';
 import logger from '../../utils/logger.js';
-import { ASSET_URLS } from './constant.js';
-import { WebsitesService } from '../websites/service.js';
-import { FormsService } from '../forms/service.js';
-import Form from '../forms/model.js';
-import axios from 'axios';
+// import { ASSET_URLS } from './constant.js';
+// import { WebsitesService } from '../websites/service.js';
+// import { FormsService } from '../forms/service.js';
+// import Form from '../forms/model.js';
+// import axios from 'axios';
 import CacheManager from '../../utils/cacheManager.js';
+import { Console } from 'winston/lib/winston/transports/index.js';
 
 /**
  * Redirects CSS asset to CDN
  */
-export const redirectCSS = (req, res, next) => {
-  try {
-    return res.redirect(302, ASSET_URLS.CSS);
-  } catch (error) {
-    next(error);
-  }
-};
+// export const redirectCSS = (req, res, next) => {
+//   try {
+//     return res.redirect(302, ASSET_URLS.CSS);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 /**
  * Redirects JS asset to CDN
  */
-export const redirectJS = (req, res, next) => {
-  try {
-    return res.redirect(302, ASSET_URLS.JS);
-  } catch (error) {
-    next(error);
-  }
-};
+// export const redirectJS = (req, res, next) => {
+//   try {
+//     return res.redirect(302, ASSET_URLS.JS);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-/**
- * Serves the links json.
- */
-export const getLinks = async (req, res, next) => {
-  try {
-    const content = {
-      css: ASSET_URLS.CSS,
-      js: ASSET_URLS.JS
-    };
-    res.set('Content-Type', 'application/json');
-    res.status(200).send(content);
-  } catch (error) {
-    next(error);
-  }
-};
-/**
- * Serves the JS asset.
- */
-export const getJs = async (req, res, next) => {
-  try {
-    const content = await getAssetContent('JS');
-    res.set('Content-Type', 'application/javascript');
-    res.send(content);
-  } catch (error) {
-    next(error);
-  }
-};
+// /**
+//  * Serves the links json.
+//  */
+// export const getLinks = async (req, res, next) => {
+//   try {
+//     const content = {
+//       css: ASSET_URLS.CSS,
+//       js: ASSET_URLS.JS
+//     };
+//     res.set('Content-Type', 'application/json');
+//     res.status(200).send(content);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+// /**
+//  * Serves the JS asset.
+//  */
+// export const getJs = async (req, res, next) => {
+//   try {
+//     const content = await getAssetContent('JS');
+//     res.set('Content-Type', 'application/javascript');
+//     res.send(content);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 /**
  * Serves the CSS asset.
  */
-export const getCss = async (req, res, next) => {
-  try {
-    // const data = {
-    // protocol: req.protocol,
-    // domain: req.get('host'),
-    // hostname: req.hostname,
-    // page: req.originalUrl,
-    // path: req.path,
-    // method: req.method
-    // };
+// export const getCss = async (req, res, next) => {
+//   try {
+//     // const data = {
+//     // protocol: req.protocol,
+//     // domain: req.get('host'),
+//     // hostname: req.hostname,
+//     // page: req.originalUrl,
+//     // path: req.path,
+//     // method: req.method
+//     // };
 
-    // console.log(data);
-    const content = await getAssetContent('CSS');
-    res.set('Content-Type', 'text/css');
-    res.send(content);
-  } catch (error) {
-    next(error);
-  }
-};
+//     // console.log(data);
+//     const content = await getAssetContent('CSS');
+//     res.set('Content-Type', 'text/css');
+//     res.send(content);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 /**
  * Extracts domain and path from the Referer header.
@@ -116,19 +117,9 @@ export const getQuoteForm = async (req, res, next) => {
     // Extract domain and path from the Referer header
     // This identifies which website is embedding our form
     const referer = req.get('referer');
+    const origin = req.get('origin') || null;
+    console.log("origin",origin);
     const { domain, path } = parseReferer(referer);
-
-    const debugData = {
-      referer,
-      extractedDomain: domain,
-      extractedPath: path,
-      requestHost: req.get('host'),
-      requestHostname: req.hostname,
-      requestPath: req.path,
-      method: req.method
-    };
-
-    logger.info('[Assets] Quote form request details', debugData);
 
     // If no referer or invalid referer, we cannot determine which form to serve
     if (!domain) {
@@ -147,11 +138,19 @@ export const getQuoteForm = async (req, res, next) => {
       });
     }
 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Explicitly allow GET
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Allow browser to send Origin header
+    res.setHeader('Vary', 'Origin');
+
+    // Referrer policy that still allows origin
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
     // Serve the JavaScript file directly from cache
     res.set('Content-Type', 'application/javascript');
     res.send(content);
-    
-    logger.info(`[Assets] Successfully served ${content.length} bytes for domain: ${domain || 'default'}`);
   } catch (error) {
     logger.error(`[Assets] Error serving quote-form.iife.js: ${error.message}`, { stack: error.stack });
     next(error);
--- modules/assets/routers/index.js
+++ modules/assets/routers/index.js
@@ -3,19 +3,19 @@ import * as controller from '../controller.js';
 
 const router = express.Router();
 
-router.get('/css', controller.redirectCSS);
-router.get('/js', controller.redirectJS);
+// router.get('/css', controller.redirectCSS);
+// router.get('/js', controller.redirectJS);
 
-router.get('/links', controller.getLinks);
+// router.get('/links', controller.getLinks);
 
-router.get('/script.js', controller.getJs);
-router.get('/style.css', controller.getCss);
+// router.get('/script.js', controller.getJs);
+// router.get('/style.css', controller.getCss);
 
 // Serve quote-form.iife.js based on domain mapping
 router.get('/quote-form.iife.js', controller.getQuoteForm);
 
 // Aliases for drop-in replacement
-router.get('/appy-tier3.js', controller.getJs);
-router.get('/appy-tier3.css', controller.getCss);
+// router.get('/appy-tier3.js', controller.getJs);
+// router.get('/appy-tier3.css', controller.getCss);
 
 export default router;
